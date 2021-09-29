dc55ba35250cd71caf30daf71ed45a5e0296c838
Robin Jeng <robinjeng0@gmail.com>
2021-09-29 13:45:40 -0700

    Add question_id to answers table, add answer_id to photos table, load schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,6 @@ CREATE DATABASE qa;
 USE QA;
 
 CREATE TABLE questions (
-  product_id INT UNIQUE NOT NULL,
   id INT UNIQUE NOT NULL,
   body VARCHAR(1000) NOT NULL,
   date TIMESTAMP NOT NULL,
@@ -23,6 +22,7 @@ CREATE TABLE questions (
 
 CREATE TABLE answers (
   id INT UNIQUE NOT NULL,
+  question_id INT UNIQUE NOT NULL,
   page INT NOT NULL,
   count INT NOT NULL,
   body VARCHAR(500) NOT NULL,
@@ -35,39 +35,8 @@ CREATE TABLE answers (
 
 CREATE TABLE photos (
   id INT UNIQUE NOT NULL,
+  answer_id INT UNIQUE NOT NULL,
   url VARCHAR(2048) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (answer_id) REFERENCES answers (id)
 );
-
-  -------------------------------------------------------------------------------
-  -- GET /qa/questions
-  -- Retrieves a list of questions for a particular product.
-  -- This list does not include any reported questions.
-  --
-  -- POST /qa/questions
-  -- Adds a question for the given product
-  --
-  -- PUT /qa/questions/:question_id/helpful
-  -- Updates a question to show it was found helpful
-  --
-  -- PUT /qa/questions/:question_id/report
-  -- Updates a question to show it was reported. Note, this action
-  -- does not delete the question, but the question will not be returned
-  -- in the above GET request.
-  -------------------------------------------------------------------------------
-  -- GET /qa/questions/:question_id/answers
-  -- Returns answers for a given question. This list does not include any reported answers.
-  --
-  -- POST /qa/questions/:question_id/answers
-  -- Adds an answer for the givin question
-  --
-  -- PUT /qa/answers/:answer_id/helpful
-  -- Updates an answer to show it was found helpful
-  --
-  -- PUT /qa/answers/:answer_id/report
-  -- Updates an answer to show it has been reported. Note, this action
-  -- does not delete the answer, but the answer will not be returned
-  -- in the above GET request.
-  -------------------------------------------------------------------------------
-
