b8c175fa4b54a180f877b10bcccedda799025fcb
Robin Jeng <robinjeng0@gmail.com>
2021-09-30 09:02:25 -0700

    Load questions data to questions table

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,13 @@
 -- show tables;
 -- describe questions/answers/photos;
 -- select * from questions/answers/photos;
+-- select * from questions limit 3;
+-- id,product_id,body,date_written,asker_name,asker_email,reported,helpful
+-- 1,1,"What fabric is the top made of?",1595884714409,"yankeelover","first.last@gmail.com",0,1
+-- add new column for date
+-- ALTER TABLE questions ADD newdate DATETIME;
+-- convert unix time from date_written to newdate
+-- update questions set newdate=from_unixtime(date_written/1000);
 
 DROP DATABASE IF EXISTS qa;
 CREATE DATABASE qa;
@@ -15,13 +22,17 @@ USE QA;
 
 CREATE TABLE questions (
   id INT UNIQUE NOT NULL,
+  product_id INT NOT NULL,
   body VARCHAR(1000) NOT NULL,
-  date TIMESTAMP NOT NULL,
-  name VARCHAR(20) UNIQUE NOT NULL,
-  helpfulness INT NOT NULL,
+  date_written  BIGINT NOT NULL,
+  asker_name VARCHAR(30) NOT NULL,
+  helpful INT NOT NULL,
   reported BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
+
+-- id,question_id,body,date_written,answerer_name,answerer_email,reported,helpful
+-- 1,36,"Supposedly suede, but I think its synthetic",1599958385988,"sillyguy","first.last@gmail.com",0,1
 
 CREATE TABLE answers (
   id INT UNIQUE NOT NULL,
