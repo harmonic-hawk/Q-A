f0d71a44e4c1e1eef1f0f014bfe5e9da92ff7cd6
Robin Jeng <robinjeng0@gmail.com>
2021-09-30 09:15:09 -0700

    Add notes for schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,9 @@
--- relational database MySQL
--- start mysql
--- mysql -u root -p
--- load schema to mysql
--- mysql -u root -p < schema.sql
--- show databases;
--- use qa;
--- show tables;
--- describe questions/answers/photos;
--- select * from questions/answers/photos;
--- select * from questions limit 3;
--- id,product_id,body,date_written,asker_name,asker_email,reported,helpful
--- 1,1,"What fabric is the top made of?",1595884714409,"yankeelover","first.last@gmail.com",0,1
--- add new column for date
--- ALTER TABLE questions ADD newdate DATETIME;
--- convert unix time from date_written to newdate
--- update questions set newdate=from_unixtime(date_written/1000);
-
 DROP DATABASE IF EXISTS qa;
 CREATE DATABASE qa;
 USE QA;
+
+-- id,product_id,body,date_written,asker_name,asker_email,reported,helpful
+-- 1,1,"What fabric is the top made of?",1595884714409,"yankeelover","first.last@gmail.com",0,1
 
 CREATE TABLE questions (
   id INT UNIQUE NOT NULL,
--- db/schemaETL.sql
+++ db/schemaETL.sql
@@ -1,5 +1,3 @@
--- /Users/robinjeng/Downloads/hr-sfo137-project-catwalk-qa/Q-A/q&a-data/questions.csv
-
 LOAD DATA LOCAL INFILE '/Users/robinjeng/Downloads/hr-sfo137-project-catwalk-qa/Q-A/q&a-data/questions.csv' INTO TABLE questions
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -15,9 +13,4 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
--- needs revision
-LOAD DATA LOCAL INFILE '/Users/robinjeng/Downloads/hr-sfo137-project-catwalk-qa/Q-A/q&a-data/questions.csv' INTO TABLE questions
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-SET date_written = FROM_UNIXTIME(@date_written/1000);
+-- SET date_written = FROM_UNIXTIME(@date_written/1000)
