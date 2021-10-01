fa43c59ecb6c3d269a813ce64771b2b35d29b43d
Robin Jeng <robinjeng0@gmail.com>
2021-10-01 09:54:15 -0700

    Recover helper functions

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-// const express = require('express');
+const router = require('./routes.js'); // router imported here
 // const path = require('path');
-const router = require('./routes.js');
 
 const PORT = 5000;
 const app = express();
@@ -15,21 +14,17 @@ const connection = mysql.createPool({
   database : 'qa'
 });
 
-
-
-
 // app.use(express.json());
 
-// app.use('/', router);
+app.use('/', router);
 // app.use(express.static('client/public'));
 
 // app.use('/api', router);
 
 // app.get('*', (req, res) => {
-//   res.send('Hello from the server!'
-//   );
+//   res.send('Hello from the server!');
   // res.sendFile(path.join(__dirname, '../client/public/index.html'));
-//   // app.use(express.static('client/public'));
+  // app.use(express.static('client/public'));
 // });
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-// const helpers = require('./helpers.js');
+const helpers = require('./helpers.js');
 // const TOKEN = require('./config.js');
 
 /// /////////////////////////////////////////
@@ -48,31 +48,31 @@ router.put('/reviews/:review_id/report', (req, res) => { helpers.reportReview(re
 
 // GET request that retrieves a questions for a particular product. This list doesn't include
 // any reported questions. product_id required; other params: pg count (default 1) & count per page
-router.get('/qa/questions', (req, res) => { });
+router.get('/qa/questions', (req, res) => { helpers.getQuestions(req, res); });
 
 // GET request that returns answers for a given question. This doesn't include any reported answers
-router.get('/qa/questions/:question_id/answers', (req, res) => { });
+router.get('/qa/questions/:question_id/answers', (req, res) => { helpers.getAnswers(req, res); });
 
 // POST request that adds a question to a product; body should include body, name, email, product_id
-router.post('/qa/questions', (req, res) => { });
+router.post('/qa/questions', (req, res) => { helpers.createQuestion(req, res); });
 
 // POST request that adds an answer to a given question; param = question_id;
 // req.body should include: body, name, email, photos.
-router.post('/qa/questions/:question_id/answers', (req, res) => { });
+router.post('/qa/questions/:question_id/answers', (req, res) => { helpers.createAnswer(req, res); });
 
 // PUT request that updates a question to show it was found helpful.
-router.put('/qa/questions/:question_id/helpful', (req, res) => { });
+router.put('/qa/questions/:question_id/helpful', (req, res) => { helpers.markQuestionAsHelpful(req, res); });
 
 // PUT request that updates a question to show it was reported.This action does not delete
 // the question, but the question will not be returned in the above GET request.
-router.put('/qa/questions/:question_id/report', (req, res) => { });
+router.put('/qa/questions/:question_id/report', (req, res) => { helpers.reportQuestion(req, res); });
 
 // PUT request that updates an answer to show it was found helpful.
-router.put('/qa/answers/:answer_id/helpful', (req, res) => { });
+router.put('/qa/answers/:answer_id/helpful', (req, res) => { helpers.markAnswerAsHelpful(req, res); });
 
 // PUT request that updates an answer to show it has been reported. Note, this action does not
 // delete the answer, but the answer will not be returned in the above GET request.
-router.put('/qa/answers/:answer_id/report', (req, res) => { });
+router.put('/qa/answers/:answer_id/report', (req, res) => { helpers.reportAnswer(req, res); });
 
 /// /////////////////////////////////////////
 /// //////////    CART API     //////////////
