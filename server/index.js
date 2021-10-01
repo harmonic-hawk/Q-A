350fb15040d0185c9035f438fa78f539bf191738
Robin Jeng <robinjeng0@gmail.com>
2021-10-01 09:48:20 -0700

    Partial server, install mysql

--- server/index.js
+++ server/index.js
@@ -1,21 +1,36 @@
-/* eslint-disable no-console */
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+// const express = require('express');
+// const path = require('path');
 const router = require('./routes.js');
 
 const PORT = 5000;
 const app = express();
 
-app.use(express.json());
-app.use(express.static('client/public'));
-
-app.use('/api', router);
-
-app.get('*', (req, res) => {
-  // res.send('Hello from the server!');
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-  // app.use(express.static('client/public'));
+const connection = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'qa'
 });
+
+
+
+
+// app.use(express.json());
+
+// app.use('/', router);
+// app.use(express.static('client/public'));
+
+// app.use('/api', router);
+
+// app.get('*', (req, res) => {
+//   res.send('Hello from the server!'
+//   );
+  // res.sendFile(path.join(__dirname, '../client/public/index.html'));
+//   // app.use(express.static('client/public'));
+// });
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
 });
